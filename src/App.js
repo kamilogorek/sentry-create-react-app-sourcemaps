f998918b2417022573ac7a5279c88ca1b23c765e
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-07-25 11:18:57 +0200

    Sourcemaps example

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function missingFunction() {
+  throw new Error('whoops');
+}
+
 function App() {
+  missingFunction();
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  debug: true,
+  dsn: 'https://363a337c11a64611be4845ad6e24f3ac@sentry.io/297378',
+  release: '1337'
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
